62be70025a4ad95ea0ff05b614bbac02c2298f4e
tommyvtran123 <46581856+tommyvtran123@users.noreply.github.com>
2021-12-10 18:12:18 -0800

    quick sort is working. Able to sort by ID

--- Final/q2.cpp
+++ Final/q2.cpp
@@ -37,28 +37,28 @@ public:
     }
 }; 
 
-int partition(int arr[], int first, int last)
+int partition(Course arr[],int first, int last)
 {
-	int pivot = arr[last];
+  Course pivot = arr[last];
 	int i = -1;
 	for (int j = 0; j < last; j++)
 	{
-		if (arr[j] < pivot)
+		if (arr[j].getSid() < pivot.getSid())
 			swap(arr[++i], arr[j]);
 	}
 	swap(arr[i + 1], arr[last]);
 	return i + 1;
 }
-void qsort(int arr[], int first, int last)
+void qsort(Course arr[], int first, int last)
 {
 	  int pivot_idx;
 
 	  if ( first >= last )
 		return;
 	
-  pivot_idx = partition(arr,first, last);
-	qsort(arr, first, pivot_idx-1);
-  qsort(arr, pivot_idx+1, last); 
+    pivot_idx = partition(arr,first, last);
+	  qsort(arr, first, pivot_idx-1);
+    qsort(arr, pivot_idx+1, last); 
 }
 
 void printArray(Course arr[], int N)
@@ -66,9 +66,9 @@ void printArray(Course arr[], int N)
 
 	for (int i = 0; i < N; i++)
   {
-		cout << arr[i].getSid()<< "\t";
-    cout << arr[i].getSname()<< "t";
-    cout << arr[i].getCredit()<< "t";  
+		    cout<<"Course Name: " << arr[i].getSname()<< endl;
+        cout <<"Course ID: "<< arr[i].getSid()<< endl;
+        cout <<"Course Credit: "<< arr[i].getCredit() << endl;  
   }
     
     cout << endl;
@@ -78,62 +78,62 @@ int main()
 {
 	Course arr[10];
 	// Course s[10];
+    cout << "Courses before sort: "<< endl;
     
     arr[0].setCid(2);
     arr[0].setCname("Calculus");
     arr[0].setCcredit(0);
-    arr[0].printCourse();
     
-    arr[1].setCid(6);
-    arr[1].setCname("trig");
-    arr[1].setCcredit(1);
-    arr[1].printCourse();
     
-    arr[2].setCid(8);
-    arr[2].setCname("History");
-    arr[2].setCcredit(2);
-    arr[2].printCourse();
+    arr[8].setCid(6);
+    arr[8].setCname("trig");
+    arr[8].setCcredit(1);
     
-    arr[3].setCid(1);
-    arr[3].setCname("Health");
-    arr[3].setCcredit(3);
-    arr[3].printCourse();
     
-    arr[4].setCid(9);
-    arr[4].setCname("Chemistry");
-    arr[4].setCcredit(4);
-    arr[4].printCourse();
+    arr[1].setCid(0);
+    arr[1].setCname("History");
+    arr[1].setCcredit(2);
+    
+    
+    arr[2].setCid(1);
+    arr[2].setCname("Health");
+    arr[2].setCcredit(3);
+    
+    
+    arr[9].setCid(9);
+    arr[9].setCname("Chemistry");
+    arr[9].setCcredit(4);
+    
     
     arr[5].setCid(4);
     arr[5].setCname("Biology");
     arr[5].setCcredit(5);
-    arr[5].printCourse();
+   
     
     arr[6].setCid(7);
     arr[6].setCname("Psychology");
     arr[6].setCcredit(6);
-    arr[6].printCourse();
+    
     
     arr[7].setCid(8);
     arr[7].setCname("Speech");
     arr[7].setCcredit(7);
-    arr[7].printCourse();
+   
     
-    arr[8].setCid(5);
-    arr[8].setCname("Physics");
-    arr[8].setCcredit(8);
-    arr[8].printCourse();
+    arr[4].setCid(5);
+    arr[4].setCname("Physics");
+    arr[4].setCcredit(8);
     
-    arr[9].setCid(3);
-    arr[9].setCname("English");
-    arr[9].setCcredit(9);
-    arr[9].printCourse();
+    
+    arr[3].setCid(3);
+    arr[3].setCname("English");
+    arr[3].setCcredit(9);
+    
   
   
   const int N = 10;
-
 	printArray(arr, N);
-	//qsort(arr,0, N-1); 
+  qsort(arr,0, N-1); 
 	printArray(arr, N);
 
 }
